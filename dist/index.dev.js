146c3b7df0ca056edc004004813fd7a50de8f869
KhepriSol <118213369+KhepriSol@users.noreply.github.com>
2023-05-09 19:49:18 -0500

    index.js
    function that prompts the user with questions to generate a README file for a project, using the Inquirer package and a markdown generator function. It then writes the generated data to a file using the file system package.

---
+++ dist/index.dev.js
@@ -0,0 +1,130 @@
+"use strict";
+
+var fs = require('fs');
+
+var inquirer = require('inquirer');
+
+var generatePage = require('./utils/generateMarkdown.js'); // Define an array of questions for the user
+
+
+var questions = function questions() {
+  // Use Inquirer to prompt questions to the user
+  return inquirer.prompt([{
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+    validate: function validate(nameInput) {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub username!');
+        return false;
+      }
+    }
+  }, {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+    validate: function validate(nameInput) {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your email address!');
+        return false;
+      }
+    }
+  }, {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project name?',
+    validate: function validate(nameInput) {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your project name!');
+        return false;
+      }
+    }
+  }, {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project.',
+    validate: function validate(nameInput) {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter a description of your project!');
+        return false;
+      }
+    }
+  }, {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'GNU'],
+    "default": 'MIT',
+    validate: function validate(nameInput) {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please choose a license!');
+        return false;
+      }
+    }
+  }, {
+    type: 'input',
+    name: 'install',
+    message: 'What are the steps required to install your project?',
+    validate: function validate(nameInput) {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter steps required to install your project!');
+        return false;
+      }
+    }
+  }, {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this app?',
+    validate: function validate(nameInput) {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter a usage description!');
+        return false;
+      }
+    }
+  }, {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    "default": 'npm test'
+  }, {
+    type: 'input',
+    name: 'contributors',
+    message: 'What does the user need to know about contributing to the repo?'
+  }]);
+}; // Define a function to write the README file using the file system
+
+
+var writeFile = function writeFile(data) {
+  fs.writeFile('README.md', data, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log('Your README has been successfully created!');
+    }
+  });
+}; // Call the questions function to initialize the program
+
+
+questions().then(function (answers) {
+  return generatePage(answers);
+}).then(function (data) {
+  return writeFile(data);
+})["catch"](function (err) {
+  return console.log(err);
+});
+//# sourceMappingURL=index.dev.js.map
--- index.js
+++ index.js
@@ -1,13 +1,136 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const generatePage = require('./utils/generateMarkdown.js');
+// Define an array of questions for the user
+const questions = () => {
+    // Use Inquirer to prompt questions to the user
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+          }
+        } 
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your email address!');
+            return false; 
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project name?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your project name!');
+            return false; 
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter a description of your project!');
+            return false; 
+          }
+        }
+      }, 
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GNU'],
+        default: 'MIT',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please choose a license!');
+            return false; 
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps required to install your project?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter steps required to install your project!');
+            return false; 
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this app?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter a usage description!');
+            return false; 
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'test', 
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+      },
+      {
+        type: 'input',
+        name: 'contributors',
+        message: 'What does the user need to know about contributing to the repo?'
+      }
+    ]);
+  };
+  
+  // Define a function to write the README file using the file system
+  const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+ 
+      if (err) {
+        console.log(err);
+        return;
+      } else {
+        console.log('Your README has been successfully created!');
+      }
+    });
+  }; 
+  
+  // Call the questions function to initialize the program
+  questions()
+    .then(answers => generatePage(answers))
+    .then(data => writeFile(data))
+    .catch(err => console.log(err));
+  
